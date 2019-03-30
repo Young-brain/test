044e5d74efae3f4fec48474742ece1c17c6caba1
Victor <9516955386@mail.ru>
2019-03-30 16:22:12 +0300

    Это наш первый комит

---
+++ 1/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+let a = 5;
+console.log(a);
+// console.log(4 / 0);
+// console.log("stirng" * 9);
+
+// let something;
+// console.log( something );
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[0]);
+
+// // alert("Hello, world!");
+// // let answer = +prompt("Есть ли вам 18 лет?", "Да");
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log( 4 + "- object");
+
+let incr = 10,
+    decr = 10;
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+
+    console.log("2" == 2);
+    console.log("2" === 2);
+
+    let isChecked = true,
+        isClose = true;
+
+    console.log(isChecked && !isClose);  
